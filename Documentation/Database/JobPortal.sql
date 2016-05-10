cb10b4ee8cb1ae3cb87dea45187f7dce2c57836e
Sreelal <sreelal.ub@gmail.com>
2016-05-10 14:19:21 +0530

    Database fix

--- Documentation/Database/JobPortal.sql
+++ Documentation/Database/JobPortal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2016 at 01:43 PM
+-- Generation Time: May 10, 2016 at 08:47 AM
 -- Server version: 5.7.9
 -- PHP Version: 5.6.16
 
@@ -19,6 +19,53 @@ SET time_zone = "+00:00";
 --
 -- Database: `jobportal`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `adm_id` int(20) NOT NULL,
+  `adm_name` varchar(100) NOT NULL,
+  `adm_pass` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adm_id`, `adm_name`, `adm_pass`) VALUES
+(1, 'admin', 'password');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `application`
+--
+
+DROP TABLE IF EXISTS `application`;
+CREATE TABLE IF NOT EXISTS `application` (
+  `apply_id` int(20) NOT NULL AUTO_INCREMENT,
+  `user_id` int(20) DEFAULT NULL,
+  `emp_id` int(20) DEFAULT NULL,
+  `job_id` int(20) DEFAULT NULL,
+  `status` int(20) DEFAULT NULL,
+  `date_applied` varchar(20) NOT NULL,
+  PRIMARY KEY (`apply_id`),
+  KEY `user_id` (`user_id`),
+  KEY `emp_id` (`emp_id`),
+  KEY `job_id` (`job_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `application`
+--
+
+INSERT INTO `application` (`apply_id`, `user_id`, `emp_id`, `job_id`, `status`, `date_applied`) VALUES
+(6, 8, 1, 2, NULL, '18-04-16');
 
 -- --------------------------------------------------------
 
@@ -39,16 +86,18 @@ CREATE TABLE IF NOT EXISTS `employer` (
   `phone` varchar(100) DEFAULT NULL,
   `location` varchar(200) DEFAULT NULL,
   `profile` varchar(700) DEFAULT NULL,
+  `logo` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`eid`),
   KEY `log_id` (`log_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employer`
 --
 
-INSERT INTO `employer` (`eid`, `log_id`, `ename`, `etype`, `industry`, `address`, `pincode`, `executive`, `phone`, `location`, `profile`) VALUES
-(1, 18, 'Infosys Pvt Ltd', 'Company', 'Software Services', 'Infosys,\r\nIT Zone,\r\n4 - BE,\r\nBangalore', '458796', 'Ajith', '9145512345', 'India,Karnataka,Bengaluru', 'Infosys is a global leader in consulting, technology, and outsourcing and next-generation services. We enable clients in more than 50 countries to outperform the competition and stay ahead of the innovation curve.');
+INSERT INTO `employer` (`eid`, `log_id`, `ename`, `etype`, `industry`, `address`, `pincode`, `executive`, `phone`, `location`, `profile`, `logo`) VALUES
+(1, 18, 'Infosys Pvt Ltd', 'Company', 'Software Services', 'Infosys,\r\nIT Zone,\r\n4 - BE,\r\nBangalore', '458796', 'Ajith', '9145512345', 'India,Karnataka,Bengaluru', 'Infosys is a global leader in consulting, technology, and outsourcing and next-generation services. We enable clients in more than 50 countries to outperform the competition and stay ahead of the innovation curve.', 'Infosys Pvt Ltd1.jpg'),
+(2, 23, 'Microsoft', 'Company', 'Software Services', 'Microsoft, Bangalore, Karnataka', '456987', 'Arun', '78945612332', 'India,Karnataka,Bommasandra', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -74,14 +123,16 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   `postdate` varchar(20) NOT NULL,
   PRIMARY KEY (`jobid`),
   KEY `eid` (`eid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `jobs`
 --
 
 INSERT INTO `jobs` (`jobid`, `eid`, `title`, `jobdesc`, `vacno`, `experience`, `basicpay`, `fnarea`, `location`, `industry`, `ugqual`, `pgqual`, `profile`, `postdate`) VALUES
-(2, 1, 'Network Administrator', 'Consulting with clients to specify system requirements and design solutions\r\nBudgeting for equipment and assembly costs\r\nAssembling new systems\r\nMaintaining existing software and hardware and upgrading any that have become obsolete\r\nWorking in tandem with IT support personnel\r\nProviding network administration and support', 3, '7', 'Rs75000', 'Network Administration', 'India,Karnataka,Bengaluru', 'Software Services', 'B.Tech/B.E.', 'M.Tech', 'Patience,\r\nTechnical skills.\r\nIT skills,\r\nInterpersonal skills,\r\nEnthusiasm,\r\nTeamworking skills,\r\nInitiative,\r\nCommitment to quality,\r\nAttention to detail.', '09-04-16');
+(2, 1, 'Network Administrator', 'Consulting with clients to specify system requirements and design solutions\r\nBudgeting for equipment and assembly costs\r\nAssembling new systems\r\nMaintaining existing software and hardware and upgrading any that have become obsolete\r\nWorking in tandem with IT support personnel\r\nProviding network administration and support', 3, '7', 'Rs75000', 'Network Administration', 'India,Karnataka,Bengaluru', 'Software Services', 'B.Tech/B.E.', 'M.Tech', 'Patience,\r\nTechnical skills.\r\nIT skills,\r\nInterpersonal skills,\r\nEnthusiasm,\r\nTeamworking skills,\r\nInitiative,\r\nCommitment to quality,\r\nAttention to detail.', '09-04-16'),
+(3, 1, 'Software Engineer', 'The focus of this position is the design and development of the core V-PIL services infrastructure, including custom automation software, job schedulers, data analysis, data visualization, and web development.', 3, '5', 'Rs 1000000', 'Network Virtualizing', 'India,Karnataka,Bengaluru', 'Software Services', 'B.Tech/B.E.', 'M.Tech', 'Strong ability in JavaScript.\r\nStrong ability in database design.\r\nExperience developing and executing performance test suites.\r\nStrong test suite development, execution and automation experience.\r\nFamiliarity with Jenkins and knowledge of existing cloud test suites, including Tempest and Rally.\r\nExperience with continuous integration practices.\r\nExperience with Juju, Charms and MAAS.', '15-04-16'),
+(4, 2, 'Web Developer', 'Development of interactive websites at microfost', 5, '3', 'Rs 25000', 'Web Development', 'India,Kerala,Ernakulam', 'Software Services', 'B.Tech/B.E.', 'Not Pursuing Post Graduation', 'Knowledge in ASP.NET, SQL server', '16-04-16');
 
 -- --------------------------------------------------------
 
@@ -106,16 +157,17 @@ CREATE TABLE IF NOT EXISTS `jobseeker` (
   `photo` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   KEY `log_id` (`log_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `jobseeker`
 --
 
 INSERT INTO `jobseeker` (`user_id`, `log_id`, `name`, `phone`, `location`, `experience`, `skills`, `basic_edu`, `master_edu`, `other_qual`, `dob`, `Resume`, `photo`) VALUES
-(7, 14, 'Akshay V K', '7894561231', 'India,West Bengal,Kalaikunda', '5', 'construction , Tax ', 'Not Pursuing Graduation', 'Not Pursuing Post Graduation', NULL, NULL, NULL, ''),
-(8, 20, 'Sreelal C', '8943202726', 'India,Kerala,Kozhikode', '1', 'Experience in Php development , HTML , MYSQL, Ajax', 'B.Tech/B.E.', 'Not Pursuing Post Graduation', NULL, NULL, NULL, NULL),
-(9, 21, 'abc', '1234567890', 'Iceland,Austurland,Bakkafjor ur', '1', 'sjndsnn,mnkjlnlnl  jnn ', 'B.A', 'CA', NULL, NULL, NULL, NULL);
+(7, 14, 'Akshay V K', '7894561231', 'India,West Bengal,Kalaikunda', '5', 'construction , Tax ', 'Not Pursuing Graduation', 'Not Pursuing Post Graduation', NULL, NULL, NULL, 'Akshay V K7.jpg'),
+(8, 20, 'Sreelal C', '8943202726', 'India,Kerala,Kozhikode', '1', 'Experience in Php development , HTML , MYSQL, Ajax', 'B.Tech/B.E.', 'Not Pursuing Post Graduation', NULL, NULL, 'Sreelal C8.docx', 'Sreelal C8.JPG'),
+(9, 21, 'abc', '1234567890', 'Iceland,Austurland,Bakkafjor ur', '1', 'sjndsnn,mnkjlnlnl  jnn ', 'B.A', 'CA', NULL, NULL, NULL, NULL),
+(10, 22, 'jishnu k s', '9526919061', 'India,Kerala,Sulthan Bathery', '9+', 'engineering at kmct', 'B.Tech/B.E.', 'MBA/PGDM', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -134,7 +186,7 @@ CREATE TABLE IF NOT EXISTS `login` (
   UNIQUE KEY `email` (`email`),
   KEY `log_id` (`log_id`),
   KEY `log_id_2` (`log_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login`
@@ -144,11 +196,41 @@ INSERT INTO `login` (`log_id`, `email`, `password`, `usertype`, `status`) VALUES
 (14, 'akshay@gmail.com', '$2y$10$3/cBmurjZzBYUkaLYE.Y..skkTdBT/YVCZb51Q3yWx73xd.Eyr13e', 'jobseeker', 1),
 (18, 'info@infosys.com', '$2y$10$/TP7ishP6SRCroPNfWcVqO1V0mMH47X.Qsft1u/Ed9xFtmietk2ga', 'employer', 0),
 (20, 'sreelal.c@live.com', '$2y$10$MfycE3o6lgrM92f5sB8kPu7c38XQkT6FeL5YF3pgx/MM/Jy12xM5i', 'jobseeker', 1),
-(21, 'abc@gmail.com', '$2y$10$ZWYhKrFT9B9m0CaysgRy5e1XMZ/e130v0LGkqw4QpkXbJ3WIV.YYe', 'jobseeker', 1);
+(21, 'abc@gmail.com', '$2y$10$ZWYhKrFT9B9m0CaysgRy5e1XMZ/e130v0LGkqw4QpkXbJ3WIV.YYe', 'jobseeker', 1),
+(22, 'jishnnuks@live.com', '$2y$10$VKC/bSdNBZWJ6PrOwnJ6xezAj1aq5VioW9YjFUsjxnAJHUkZHRWBq', 'jobseeker', 1),
+(23, 'info@microsoft.com', '$2y$10$q.xafcSTYUoKtz2FIhrf7OX1x0weMZRzY3beiqoO2NGe0PUKJlzga', 'employer', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `selection`
+--
+
+DROP TABLE IF EXISTS `selection`;
+CREATE TABLE IF NOT EXISTS `selection` (
+  `sel_id` int(20) NOT NULL AUTO_INCREMENT,
+  `user_id` int(20) DEFAULT NULL,
+  `emp_id` int(20) DEFAULT NULL,
+  `job_id` int(20) DEFAULT NULL,
+  `status` int(5) DEFAULT NULL,
+  `date` varchar(20) NOT NULL,
+  PRIMARY KEY (`sel_id`),
+  KEY `user_id` (`user_id`),
+  KEY `emp_id` (`emp_id`),
+  KEY `job_id` (`job_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `application`
+--
+ALTER TABLE `application`
+  ADD CONSTRAINT `fk_empid` FOREIGN KEY (`emp_id`) REFERENCES `employer` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_job` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`jobid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_userid` FOREIGN KEY (`user_id`) REFERENCES `jobseeker` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `employer`
@@ -167,6 +249,14 @@ ALTER TABLE `jobs`
 --
 ALTER TABLE `jobseeker`
   ADD CONSTRAINT `fk_login` FOREIGN KEY (`log_id`) REFERENCES `login` (`log_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `selection`
+--
+ALTER TABLE `selection`
+  ADD CONSTRAINT `fk_emp` FOREIGN KEY (`emp_id`) REFERENCES `employer` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_jobs` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`jobid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `jobseeker` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
