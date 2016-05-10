7a61ef7543c3989072eefaa9348ff1a4fa007f6a
Sreelal <sreelal.ub@gmail.com>
2016-05-10 19:34:33 +0530

    added license information
    added license information to all files

--- login.php
+++ login.php
@@ -4,7 +4,23 @@
 * User: Sreelal
 * Date: 2-04-2016
 * Time: 8:03 PM
-*/
+ * Online-Job-Portal - A web application built on PHP HTML & javascript
+Copyright (C) 2016 Sreelal C
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
 ?>
 <!DOCTYPE html>
 <html lang="en">
